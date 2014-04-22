9fa18725a93608ad49bd80251c4ec2c3c9ea903f
Igonina Galina <igonina2101@gmail.com>
2014-04-23 00:07:43 +1100

    Создала производителя и связала его с моделью, теперь при создании машины добавляются созданные производитель и модель машины

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 
 var users = [];
 var manufacturers = [];
+var fuel = [];
+var repair = [];
+var providers = [];
 
 users.list = function() {
 	if(!(this.length>0)) {
@@ -34,8 +37,8 @@ app.get('/user/add', function(req, res){
 
 app.get('/user/:id/cars/add', function(req, res){
 	users[req.params.id].cars.push({
-		manufacturer: Math.random(),
-		model: Math.random(),
+		manufacturer: manufacturers[0],
+		model: manufacturers[0].models[0],
 		year: Math.random(),
 		engine_num_or_vin: Math.random(),
 		cuzov_num: Math.random(),
@@ -47,9 +50,72 @@ app.get('/user/:id/cars/add', function(req, res){
 
 app.get('/manufacturer/add/:name', function(req, res) {
 	manufacturers.push({
-		name: req.params.name
+		name: req.params.name,
+		models: []
 	});
 	res.send('Производитель добавлен: ' + req.params.name);
+});
+
+app.get('/manufacturer/:id/model/add/:name', function(req, res) {
+	manufacturers[req.params.id].models.push({
+		name: req.params.name
+	});
+	res.send('Модель ' + req.params.name+ 'для производителя'+req.params.id+ 'добавлена');
+});
+
+app.get('/fuel/add', function(req, res) {
+	fuel.push({
+		consumption_km: Math.random(),
+		consumption_l: Math.random(),
+		cost_l: Math.random(),
+		date_of_filling: Math.random()
+	});
+	res.send('Расход топлива добавлен');
+});
+
+app.get('/repair/add', function(req, res) {
+	repair.push({
+		mileage: Math.random(),
+		sum: Math.random(),
+		repair_view: [] 
+	});
+	res.send('Ремонт добавлен');
+});
+		
+app.get('/repair/:id/repair_view/add/:view', function(req, res) {
+	repair[req.params.id].repair_view.push({
+		view: req.params.view		
+	});
+	res.send('Вид ремонта' + req.params.view+ '  для ремонта '+req.params.id+ '  добавлено  ');
+});
+
+app.get('/provider/add', function(req, res) {
+	providers.push({
+		name_shop: Math.random(),
+		address: Math.random(),
+		average_check: Math.random(), 
+		store: []
+	});
+	res.send('Поставщик добавлен');
+});
+
+app.get('/provider/:id/store/add', function(req, res) {
+	providers[req.params.id].store.push({
+		manufacturer: Math.random(),
+		model: Math.random(),
+		engine_num_or_vin: Math.random(),
+		details: [],
+		detail_num: Math.random(),
+		detail_cost: Math.random()				
+	});
+	res.send('Склад для поставщика' + req.params.id+  '  добавлен');
+});
+
+app.get('/store/:id/detail/add', function(req, res) {
+	store[req.params.id].detail.push({
+		name: Math.random()				
+	});
+	res.send('Деталь для склада' + req.params.id+  '  добавлена');
 });
 
 var server = app.listen(3000, function() {
